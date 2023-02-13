eabd77a160f98d3798413bdffff0bdc855e8c71d
fatouuuu <fatmaabdullrahim@gmail.com>
2023-02-13 08:40:24 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+      @word = word.downcase
+    end
+    
+    def match(possible_anagrams)
+      possible_anagrams.select { |possible_anagram| anagram?(possible_anagram) }
+    end
+    
+    private
+    
+    def anagram?(possible_anagram)
+      possible_anagram = possible_anagram.downcase
+      return false if possible_anagram == @word
+      
+      possible_anagram.chars.sort == @word.chars.sort
+    end
+end
+  
